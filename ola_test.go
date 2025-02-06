d362d4e35985e6ee4da14d469ff6cef0f748086b
Paulo Ricardo Malta Leal <prmaltaleal@gmail.com>
2025-02-06 15:25:51 -0300

    feat: add subcases

--- ola_test.go
+++ ola_test.go
@@ -3,10 +3,25 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
-	result := Ola("Paulin")
-	expected := "Olá, Paulin"
 
-	if result != expected {
-		t.Errorf("Result '%s', expected '%s'", result, expected)
+	verifyMessage := func(t testing.TB, result, expected string) {
+		t.Helper() // deixando explicito que é um metodo auxiliar
+		if result != expected {
+			t.Errorf("resultado '%q', esperado '%q'", result, expected)
+		}
 	}
+
+	t.Run("diz olá para as pessoas", func(t *testing.T) {
+		result := Ola("paulin")
+		expected := "Olá, paulin"
+
+		verifyMessage(t, result, expected)
+	})
+
+	t.Run("diz 'Olá, mundo' quando uma string vazia for passada", func(t *testing.T) {
+		result := Ola("")
+		expected := "Olá, mundo"
+
+		verifyMessage(t, result, expected)
+	})
 }
